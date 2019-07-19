f3a2ede40a0e023f7029ba25715d14e03803ed71
ozncnock <44199166+ozncnock@users.noreply.github.com>
2019-07-19 12:42:33 +0300

    Java / Swing - Calculator
    Hesap makinesi uygulaması, Swing tasarımı kullanıldı.

---
+++ src/formamacli/Formamacli.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package formamacli;
+/**
+ *
+ * @author Devgoril
+ */
+public class Formamacli {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    
+        NewJFrame a = new NewJFrame();
+        a.setVisible(true);
+    }
+    
+}
